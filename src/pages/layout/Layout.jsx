833984c44c3c3ca1f9c06c0303817d04ba0c0b3a
heechul94 <yhcu3517@gmail.com>
2024-02-09 22:35:42 +0900

    Design: 레이아웃 UI 수정

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,16 +2,23 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import styled from 'styled-components';
 
-const StyledLayout = styled.main`
+const StyledLayout = styled.div`
   max-width: 1440px;
+  height: 100vh;
   margin: 0 auto;
 `;
+
+// const OutletWrapper = styled.div`
+//   padding-top: 70px;
+// `;
 
 const Layout = ({ authState }) => {
   return (
     <StyledLayout>
       <Header authState={authState} />
       <Outlet />
+      {/* <OutletWrapper>
+      </OutletWrapper> */}
     </StyledLayout>
   );
 };
