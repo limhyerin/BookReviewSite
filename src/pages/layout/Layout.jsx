441668a31b0b651a3f3ca1a1d74e8903ec4f18db
heechul94 <yhcu3517@gmail.com>
2024-02-10 08:55:13 +0900

    Design: 레이아웃,헤더 UI 수정

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -3,22 +3,20 @@ import Header from './Header';
 import styled from 'styled-components';
 
 const StyledLayout = styled.div`
-  max-width: 1440px;
-  height: 100vh;
+  min-height: 100vh;
   margin: 0 auto;
 `;
-
-// const OutletWrapper = styled.div`
-//   padding-top: 70px;
-// `;
-
+const StyledMain = styled.main`
+  height: calc(100vh - 70px);
+  padding-top: 70px;
+`;
 const Layout = ({ authState }) => {
   return (
     <StyledLayout>
       <Header authState={authState} />
-      <Outlet />
-      {/* <OutletWrapper>
-      </OutletWrapper> */}
+      <StyledMain style={{ paddingTop: '70px', height: 'calc(100vh - 70px)' }}>
+        <Outlet />
+      </StyledMain>
     </StyledLayout>
   );
 };
