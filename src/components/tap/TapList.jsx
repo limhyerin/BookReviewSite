59cb3f28985f8d4dd028557b533d45360a5ca918
chomusuke9072 <chomusuke9072@gmail.com>
2024-02-12 08:55:42 +0900

    Feat: 마이페이지 기능 구현

---
+++ src/components/tap/TapList.jsx
@@ -0,0 +1,80 @@
+import { useNavigate } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
+
+const TapList = ({ onSelectTab, activeTab, setActiveTab }) => {
+  const navigate = useNavigate();
+  const onActiveList = (e) => {
+    if (e.target === e.currentTarget) return;
+    setActiveTab(e.target.textContent);
+  };
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    onSelectTab(tab);
+  };
+
+  const handleSingOut = async () => {
+    await signOut(auth);
+    navigate('/');
+  };
+
+  return (
+    <>
+      <TabsWrapper onClick={onActiveList}>
+        <TapListWrapper>
+          <Tap $activeTab={activeTab} onClick={() => handleTabClick('내 정보 수정')}>
+            내 정보 수정
+          </Tap>
+          <Tap $activeTab={activeTab} onClick={() => handleTabClick('내 리뷰 보기')}>
+            내 리뷰 보기
+          </Tap>
+        </TapListWrapper>
+        <Tap onClick={handleSingOut}>Sign Out</Tap>
+      </TabsWrapper>
+    </>
+  );
+};
+
+const TabsWrapper = styled.ul`
+  height: 500px;
+  width: 20rem;
+  margin: 2rem 2rem;
+  padding: 4rem 1rem;
+  background-color: #fff;
+  color: #222222;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 15rem;
+`;
+
+const TapListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const Tap = styled.li`
+  display: flex;
+  cursor: pointer;
+  font-size: 1rem;
+
+  ${(props) => {
+    if (props.$activeTab === props.children) {
+      return css`
+        color: #feeb60;
+        font-weight: bolder;
+      `;
+    }
+    return css`
+      color: #222222;
+    `;
+  }}
+`;
+
+export default TapList;
---
+++ src/components/tap/TapProfil.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Avatar from '../avatar/Avatar';
+import CustomButton from '../CustomButton';
+import { getAuth, onAuthStateChanged, updateProfile } from 'firebase/auth'; // Firebase 인증 모듈에서 필요한 함수 가져오기
+
+const TapProfil = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newNickname, setNewNickname] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+
+  // 사용자 로그인 상태 변경 감지
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user); // 로그인된 사용자가 없으면 null을 반환
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = async () => {
+    try {
+      await updateProfile(currentUser, {
+        displayName: newNickname
+      });
+      setIsEditing(false);
+      console.log('닉네임이 업데이트되었습니다.');
+    } catch (error) {
+      console.error('닉네임 업데이트 오류:', error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setNewNickname(event.target.value);
+  };
+
+  return (
+    <UserInfo>
+      <User>
+        <Avatar />
+        <div>
+          {currentUser && currentUser.displayName ? (
+            isEditing ? (
+              <NicknameInput type="text" value={newNickname} onChange={handleChange} placeholder="새로운 닉네임 입력" />
+            ) : (
+              <p>{currentUser.displayName}</p>
+            )
+          ) : (
+            <p>사용자가 로그인되어 있지 않습니다.</p>
+          )}
+        </div>
+      </User>
+      <div>
+        {currentUser && currentUser.displayName && isEditing ? (
+          <CustomButton text={'수정 완료'} onClick={handleSaveClick} />
+        ) : (
+          <CustomButton text={'수정하기'} onClick={handleEditClick} />
+        )}
+      </div>
+    </UserInfo>
+  );
+};
+
+const UserInfo = styled.div`
+  display: flex;
+  width: 20rem;
+  height: 30rem;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #fff;
+  margin: -7rem 20rem;
+  border-radius: 10px;
+  border: 1px solid #ededed;
+`;
+
+const User = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+`;
+
+const NicknameInput = styled.input`
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  outline: none;
+`;
+
+export default TapProfil;
