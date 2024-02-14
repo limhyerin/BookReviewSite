4da2033d7679b93dc9c76f47fc68eff56e2cde36
heechul94 <yhcu3517@gmail.com>
2024-02-14 11:52:14 +0900

    Fix: 안쓰는 state 수정

--- src/redux/modules/authReducer.js
+++ src/redux/modules/authReducer.js
@@ -1,17 +1,10 @@
 const SET_USERINFO = 'authReducer/SET_USERINFO';
-const SET_ISLOGGED = 'authReducer/SET_ISLOGGED';
 const UPDATE_PROFILE = 'authReducer/UPDATE_PROFILE';
 const UPDATE_NICKNAME = 'authReducer/UPDATE_NICKNAME';
 
 export const setUserInfo = (payload) => {
   return {
     type: SET_USERINFO,
-    payload
-  };
-};
-export const setIsLogged = (payload) => {
-  return {
-    type: SET_ISLOGGED,
     payload
   };
 };
@@ -29,16 +22,13 @@ export const updateNickname = (payload) => {
 };
 
 const data = {
-  userInfo: {},
-  isLogged: false
+  userInfo: {}
 };
 
 const authReducer = (state = data, action) => {
   switch (action.type) {
     case SET_USERINFO:
       return { ...state, userInfo: { ...action.payload } };
-    case SET_ISLOGGED:
-      return { ...state, isLogged: action.payload };
     case UPDATE_PROFILE:
       return { ...state, userInfo: { ...state.userInfo, profile: action.payload } };
     case UPDATE_NICKNAME:
